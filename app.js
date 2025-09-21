fe2470fdbb15c8da4a129dbd92e49d920f2bfda1
Deepak Oraon <deepakoraon0348@gmail.com>
2025-09-21 13:00:26 +0530

    Fix backend static file serving and wildcard route11

--- app.js
+++ app.js
@@ -44,9 +44,9 @@ app.use("/api/v2", list);
 // });
 
 
-app.use(express.static(path.join(__dirname, "frontend", "dist")));
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
 );
 
 app.listen(port,()=>{
